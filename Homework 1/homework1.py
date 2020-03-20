ff442a58b56a3b3702cac43c239f41a85dbedf26
Sambo Chhay <chhaysambo@gmail.com>
2020-03-20 22:34:42 +0700

    Homework 1

---
+++ Homework 1/homework1.py
@@ -0,0 +1,12 @@
+# Write a program to random print out fullname
+# Fullname = firstname + lastname
+firstnames_list = [
+    "Emma", "Olivia", "Ava", "Isabella", "Sophia", "Charlotte",
+    "Mia", "Amelia", "Harper", "Evelyn"
+]
+lastnames_list = [
+    "Abigail", "Emily", "Elizabeth", "Mila", "Ella", "Avery",
+    "Sofia", "Camila", "Aria", "Scarlett"
+]
+
+# Hint: use random to random name from list above
