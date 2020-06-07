31e74ff1ad52b803685cb1574767070da2ca4d4c
Sambo Chhay <chhaysambo@gmail.com>
2020-06-07 11:31:39 +0700

    Update import

--- Week 10/create_database.py
+++ Week 10/create_database.py
@@ -1,14 +1,5 @@
-from main import db, Contact
+from application import db
 
 db.create_all()
-
-# sok = Contact(id=1, name='Sok', phone='012', facebook='fb.com/sok')
-# sau = Contact(id=2, name='Sau', phone='013', facebook='fb.com/sau')
-# minea = Contact(id=3, name='Minea', phone='016', facebook='fb.com/minea')
-
-# db.session.add(sok)
-# db.session.add(sau)
-# db.session.add(minea)
-# db.session.commit()
 
 print("New Database Created!!!")
